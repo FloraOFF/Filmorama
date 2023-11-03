387b8305df0edf8bccf2e53e1265c52f5cf0eddc
FloraOFF <floraoliveirafranca@gmail.com>
2023-11-03 16:04:32 -0500

    Mudando for in por of para pegar elementos no js, está funcionando de fato

--- visoes/js/filmes.js
+++ visoes/js/filmes.js
@@ -2,7 +2,7 @@ const filmes = () => {
     fetch('/Filmorama/controladores/rota.php?acao=mostrarFilmes')
     .then(response => response.json())
     .then(filmes => {
-        for (const filme in filmes) {
+        for (const filme of filmes) {
             console.log (filme);
         }
     })
