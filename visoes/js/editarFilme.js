725d1afc26ddd7aad8bb318a7daa7e95df3f6b81
FloraOFF <floraoliveirafranca@gmail.com>
2023-12-06 16:49:37 -0500

    Adicionando elementos no form para editar

--- visoes/js/editarFilme.js
+++ visoes/js/editarFilme.js
@@ -3,8 +3,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const idFilme = parametrosURL.get('idFilme');
 
     if (idFilme) {
-        // O idFilme contém o valor do id passado pela URL
-        // Agora você pode usar esse valor para carregar os dados do filme e preencher o formulário de edição
         console.log(`Editando... Filme com id ${idFilme}`);
 
         fetch('/Filmorama/controladores/rota.php?acao=mostrarFilmes')
@@ -22,7 +20,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 console.error('Erro ao obter dados do filme:', error);
             });
     } else {
-        // Caso não haja idFilme na URL, você pode lidar com isso aqui
         console.error('Id do filme não encontrado na URL');
     }
 });
@@ -31,7 +28,18 @@ function preencherFormularioEdicao(filme) {
     console.log (filme);
     // Aqui você implementaria a lógica para preencher o formulário de edição
     // por exemplo:
-    // document.getElementById('titulo').value = filme.titulo;
-    // document.getElementById('dataEstreia').value = filme.dataEstreia;
-    // Preencher outros campos conforme necessário
+    // const form = document.querySelector('form');
+    
+    document.getElementById('titulo').value = filme.titulo;
+    document.getElementById('dataEstreia').value = filme.dataEstreia;
+    document.getElementById('genero').value = filme.genero;
+    document.getElementById('classificacao').value = filme.classificacao;
+    document.getElementById('duracao').value = filme.duracao;
+    document.getElementById('elenco').value = filme.elenco;
+    document.getElementById('sinopse').value = filme.sinopse;
+    document.getElementById('avaliacao').value = filme.avaliacao;
+    document.getElementById('produtora').value = filme.produtora;
+    document.getElementById('paisOrigem').value = filme.paisOrigem;
+    document.getElementById('idioma').value = filme.idioma;
+    //Preencher outros campos conforme necessário
 }
